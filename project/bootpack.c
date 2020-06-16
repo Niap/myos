de1dd7640df1ff4d75054d275cc37952fe29adc5
Niap <niap.pr@gmail.com>
2020-06-16 18:16:15 +0800

    display color on graphic card

--- project/bootpack.c
+++ project/bootpack.c
@@ -4,7 +4,7 @@ void HariMain(void)
 	int i; /* 曄悢愰尵丅i偲偄偆曄悢偼丄32價僢僩偺惍悢宆 */
 
 	for (i = 0xa0000; i <= 0xaffff; i++) {
-		write_mem8(i, 7); /* MOV BYTE [i],15 */
+		write_mem8(i, i & 0x0f);
 	}
 
 	for (;;) {
