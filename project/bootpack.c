ef5905a8d49be67775f494272a40cc73cda278c8
Niap <niap.pr@gmail.com>
2020-06-20 15:53:50 +0800

    add memory test& memory manager

--- project/bootpack.c
+++ project/bootpack.c
@@ -1,10 +1,22 @@
 #include "bootpack.h"
-extern struct FIFO8 keyfifo,mousefifo;
 
+#define MEMMAN_FREES		4090	/* ����Ŗ�32KB */
+#define MEMMAN_ADDR			0x003c0000
 
-void enable_mouse(struct MOUSE_DEC *mdec);
-void init_keyboard(void);
-int mouse_decode(struct MOUSE_DEC *mdec, unsigned char dat);
+struct FREEINFO {	/* ������� */
+	unsigned int addr, size;
+};
+
+struct MEMMAN {
+	int frees, maxfrees, lostsize, losts;
+	struct FREEINFO free[MEMMAN_FREES];
+};
+
+unsigned int memtest(unsigned int start, unsigned int end);
+void memman_init(struct MEMMAN *man);
+unsigned int memman_total(struct MEMMAN *man);
+unsigned int memman_alloc(struct MEMMAN *man, unsigned int size);
+int memman_free(struct MEMMAN *man, unsigned int addr, unsigned int size);
 
 
 void HariMain(void)
@@ -12,12 +24,21 @@ void HariMain(void)
 	struct BOOTINFO *binfo = (struct BOOTINFO *) 0x0ff0;
 	char s[40],mcursor[256], keybuf[32], mousebuf[128];
 	int mx, my,i;
-
+	unsigned int memtotal;
+	struct MEMMAN *memman = (struct MEMMAN *) MEMMAN_ADDR;
 	struct MOUSE_DEC mdec;
 
 	init_gdtidt();
 	init_pic();
 	io_sti();
+	fifo8_init(&keyfifo, 32, keybuf);
+	fifo8_init(&mousefifo, 32, mousebuf);
+	io_out8(PIC0_IMR, 0xf9); /* PIC1�ƃL�[�{�[�h������(11111001) */
+	io_out8(PIC1_IMR, 0xef); /* �}�E�X������(11101111) */
+
+	init_keyboard();
+	enable_mouse(&mdec);
+
 	init_palette();
 	init_screen8(binfo->vram, binfo->scrnx, binfo->scrny);
 	mx = (binfo->scrnx - 16) / 2; /* ��ʒ����ɂȂ�悤�ɍ��W�v�Z */
@@ -27,13 +48,19 @@ void HariMain(void)
 	sprintf(s, "(%d, %d)", mx, my);
 	putfonts8_asc(binfo->vram, binfo->scrnx, 0, 0, COL8_FFFFFF, s);
 
-	fifo8_init(&keyfifo, 32, keybuf);
-	fifo8_init(&mousefifo, 32, mousebuf);
-	io_out8(PIC0_IMR, 0xf9); /* PIC1�ƃL�[�{�[�h������(11111001) */
-	io_out8(PIC1_IMR, 0xef); /* �}�E�X������(11101111) */
+	memtotal = memtest(0x00400000, 0xbfffffff);
+	memman_init(memman);
+	memman_free(memman, 0x00001000, 0x0009e000); /* 0x00001000 - 0x0009efff */
+	memman_free(memman, 0x00400000, memtotal - 0x00400000);
 
-	init_keyboard();
-	enable_mouse(&mdec);
+	i = memtest(0x00400000, 0xbfffffff) / (1024 * 1024);
+	sprintf(s, "memory %dMB", i);
+	putfonts8_asc(binfo->vram, binfo->scrnx, 0, 32, COL8_FFFFFF, s);
+
+	sprintf(s, "memory %dMB   free : %dKB",
+			memtotal / (1024 * 1024), memman_total(memman) / 1024);
+	putfonts8_asc(binfo->vram, binfo->scrnx, 0, 32, COL8_FFFFFF, s);
+
 
 	for (;;) {
 		io_cli();
@@ -88,3 +115,139 @@ void HariMain(void)
 	}
 }
 
+
+#define EFLAGS_AC_BIT		0x00040000
+#define CR0_CACHE_DISABLE	0x60000000
+
+unsigned int memtest(unsigned int start, unsigned int end)
+{
+	char flg486 = 0;
+	unsigned int eflg, cr0, i;
+
+	/* 386���A486�ȍ~�Ȃ̂��̊m�F */
+	eflg = io_load_eflags();
+	eflg |= EFLAGS_AC_BIT; /* AC-bit = 1 */
+	io_store_eflags(eflg);
+	eflg = io_load_eflags();
+	if ((eflg & EFLAGS_AC_BIT) != 0) { /* 386�ł�AC=1�ɂ��Ă�������0�ɖ߂��Ă��܂� */
+		flg486 = 1;
+	}
+	eflg &= ~EFLAGS_AC_BIT; /* AC-bit = 0 */
+	io_store_eflags(eflg);
+
+	if (flg486 != 0) {
+		cr0 = load_cr0();
+		cr0 |= CR0_CACHE_DISABLE; /* �L���b�V���֎~ */
+		store_cr0(cr0);
+	}
+
+	i = memtest_sub(start, end);
+
+	if (flg486 != 0) {
+		cr0 = load_cr0();
+		cr0 &= ~CR0_CACHE_DISABLE; /* �L���b�V������ */
+		store_cr0(cr0);
+	}
+
+	return i;
+}
+
+
+void memman_init(struct MEMMAN *man)
+{
+	man->frees = 0;			/* �������̌� */
+	man->maxfrees = 0;		/* �󋵊ώ@�p�Ffrees�̍ő�l */
+	man->lostsize = 0;		/* ����Ɏ��s�������v�T�C�Y */
+	man->losts = 0;			/* ����Ɏ��s������ */
+	return;
+}
+
+unsigned int memman_total(struct MEMMAN *man)
+{
+	unsigned int i, t = 0;
+	for (i = 0; i < man->frees; i++) {
+		t += man->free[i].size;
+	}
+	return t;
+}
+
+unsigned int memman_alloc(struct MEMMAN *man, unsigned int size)
+{
+	unsigned int i, a;
+	for (i = 0; i < man->frees; i++) {
+		if (man->free[i].size >= size) {
+			a = man->free[i].addr;
+			man->free[i].addr += size;
+			man->free[i].size -= size;
+			if (man->free[i].size == 0) {
+				man->frees--;
+				for (; i < man->frees; i++) {
+					man->free[i] = man->free[i + 1]; 
+				}
+			}
+			return a;
+		}
+	}
+	return 0; 
+}
+
+
+int memman_free(struct MEMMAN *man, unsigned int addr, unsigned int size)
+{
+	int i, j;
+	for (i = 0; i < man->frees; i++) {
+		if (man->free[i].addr > addr) {
+			break;
+		}
+	}
+	/* free[i - 1].addr < addr < free[i].addr */
+	if (i > 0) {
+		/* �O������ */
+		if (man->free[i - 1].addr + man->free[i - 1].size == addr) {
+			/* �O�̂����̈�ɂ܂Ƃ߂��� */
+			man->free[i - 1].size += size;
+			if (i < man->frees) {
+				/* �������� */
+				if (addr + size == man->free[i].addr) {
+					/* �Ȃ�ƌ��Ƃ��܂Ƃ߂��� */
+					man->free[i - 1].size += man->free[i].size;
+					/* man->free[i]�̍폜 */
+					/* free[i]���Ȃ��Ȃ����̂őO�ւ߂� */
+					man->frees--;
+					for (; i < man->frees; i++) {
+						man->free[i] = man->free[i + 1]; /* �\���̂̑�� */
+					}
+				}
+			}
+			return 0; /* �����I�� */
+		}
+	}
+	/* �O�Ƃ͂܂Ƃ߂��Ȃ����� */
+	if (i < man->frees) {
+		/* ��낪���� */
+		if (addr + size == man->free[i].addr) {
+			/* ���Ƃ͂܂Ƃ߂��� */
+			man->free[i].addr = addr;
+			man->free[i].size += size;
+			return 0; /* �����I�� */
+		}
+	}
+	/* �O�ɂ����ɂ��܂Ƃ߂��Ȃ� */
+	if (man->frees < MEMMAN_FREES) {
+		/* free[i]�������A���ւ��炵�āA�����܂���� */
+		for (j = man->frees; j > i; j--) {
+			man->free[j] = man->free[j - 1];
+		}
+		man->frees++;
+		if (man->maxfrees < man->frees) {
+			man->maxfrees = man->frees; /* �ő�l���X�V */
+		}
+		man->free[i].addr = addr;
+		man->free[i].size = size;
+		return 0; /* �����I�� */
+	}
+	/* ���ɂ��点�Ȃ����� */
+	man->losts++;
+	man->lostsize += size;
+	return -1; /* ���s�I�� */
+}
