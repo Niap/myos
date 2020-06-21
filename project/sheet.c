13cdbd1dcbeace849f08ae5e6b0ced3dee12d378
Niap <niap.pr@gmail.com>
2020-06-21 14:37:39 +0800

    day9

--- project/sheet.c
+++ project/sheet.c
@@ -1,3 +1,5 @@
+/* �}�E�X��E�B���h�E�̏d�ˍ��킹���� */
+
 #include "bootpack.h"
 
 #define SHEET_USE		1
@@ -45,11 +47,42 @@ void sheet_setbuf(struct SHEET *sht, unsigned char *buf, int xsize, int ysize, i
 	return;
 }
 
+void sheet_refreshsub(struct SHTCTL *ctl, int vx0, int vy0, int vx1, int vy1)
+{
+	int h, bx, by, vx, vy, bx0, by0, bx1, by1;
+	unsigned char *buf, c, *vram = ctl->vram;
+	struct SHEET *sht;
+	for (h = 0; h <= ctl->top; h++) {
+		sht = ctl->sheets[h];
+		buf = sht->buf;
+		/* vx0�`vy1���g���āAbx0�`by1���t�Z���� */
+		bx0 = vx0 - sht->vx0;
+		by0 = vy0 - sht->vy0;
+		bx1 = vx1 - sht->vx0;
+		by1 = vy1 - sht->vy0;
+		if (bx0 < 0) { bx0 = 0; }
+		if (by0 < 0) { by0 = 0; }
+		if (bx1 > sht->bxsize) { bx1 = sht->bxsize; }
+		if (by1 > sht->bysize) { by1 = sht->bysize; }
+		for (by = by0; by < by1; by++) {
+			vy = sht->vy0 + by;
+			for (bx = bx0; bx < bx1; bx++) {
+				vx = sht->vx0 + bx;
+				c = buf[by * sht->bxsize + bx];
+				if (c != sht->col_inv) {
+					vram[vy * ctl->xsize + vx] = c;
+				}
+			}
+		}
+	}
+	return;
+}
+
 void sheet_updown(struct SHTCTL *ctl, struct SHEET *sht, int height)
 {
 	int h, old = sht->height; /* �ݒ�O�̍������L������ */
 
-	/* */
+	/* �w�肪�Ⴗ���⍂������������A�C������ */
 	if (height > ctl->top + 1) {
 		height = ctl->top + 1;
 	}
@@ -58,9 +91,10 @@ void sheet_updown(struct SHTCTL *ctl, struct SHEET *sht, int height)
 	}
 	sht->height = height; /* ������ݒ� */
 
-	/* sheets 重新排序 */
-	if (old > height) {
+	/* �ȉ��͎��sheets[]�̕��בւ� */
+	if (old > height) {	/* �ȑO�����Ⴍ�Ȃ� */
 		if (height >= 0) {
+			/* �Ԃ̂��̂������グ�� */
 			for (h = old; h > height; h--) {
 				ctl->sheets[h] = ctl->sheets[h - 1];
 				ctl->sheets[h]->height = h;
@@ -74,9 +108,9 @@ void sheet_updown(struct SHTCTL *ctl, struct SHEET *sht, int height)
 					ctl->sheets[h]->height = h;
 				}
 			}
-			ctl->top--;
+			ctl->top--; /* �\�����̉������������̂ŁA��ԏ�̍��������� */
 		}
-		sheet_refresh(ctl); /* �V�����������̏��ɉ����ĉ�ʂ�`������ */
+		sheet_refreshsub(ctl, sht->vx0, sht->vy0, sht->vx0 + sht->bxsize, sht->vy0 + sht->bysize);
 	} else if (old < height) {	/* �ȑO���������Ȃ� */
 		if (old >= 0) {
 			/* �Ԃ̂��̂����������� */
@@ -94,39 +128,27 @@ void sheet_updown(struct SHTCTL *ctl, struct SHEET *sht, int height)
 			ctl->sheets[height] = sht;
 			ctl->top++; /* �\�����̉��������������̂ŁA��ԏ�̍����������� */
 		}
-		sheet_refresh(ctl); /* �V�����������̏��ɉ����ĉ�ʂ�`������ */
+		sheet_refreshsub(ctl, sht->vx0, sht->vy0, sht->vx0 + sht->bxsize, sht->vy0 + sht->bysize);
 	}
 	return;
 }
 
-void sheet_refresh(struct SHTCTL *ctl)
+void sheet_refresh(struct SHTCTL *ctl, struct SHEET *sht, int bx0, int by0, int bx1, int by1)
 {
-	int h, bx, by, vx, vy;
-	unsigned char *buf, c, *vram = ctl->vram;
-	struct SHEET *sht;
-	for (h = 0; h <= ctl->top; h++) {
-		sht = ctl->sheets[h];
-		buf = sht->buf;
-		for (by = 0; by < sht->bysize; by++) {
-			vy = sht->vy0 + by;
-			for (bx = 0; bx < sht->bxsize; bx++) {
-				vx = sht->vx0 + bx;
-				c = buf[by * sht->bxsize + bx];
-				if (c != sht->col_inv) {
-					vram[vy * ctl->xsize + vx] = c;
-				}
-			}
-		}
+	if (sht->height >= 0) { /* �������\�����Ȃ�A�V�����������̏��ɉ����ĉ�ʂ�`������ */
+		sheet_refreshsub(ctl, sht->vx0 + bx0, sht->vy0 + by0, sht->vx0 + bx1, sht->vy0 + by1);
 	}
 	return;
 }
 
 void sheet_slide(struct SHTCTL *ctl, struct SHEET *sht, int vx0, int vy0)
 {
+	int old_vx0 = sht->vx0, old_vy0 = sht->vy0;
 	sht->vx0 = vx0;
 	sht->vy0 = vy0;
-	if (sht->height >= 0) { /* �������\�����Ȃ� */
-		sheet_refresh(ctl); /* �V�����������̏��ɉ����ĉ�ʂ�`������ */
+	if (sht->height >= 0) { /* �������\�����Ȃ�A�V�����������̏��ɉ����ĉ�ʂ�`������ */
+		sheet_refreshsub(ctl, old_vx0, old_vy0, old_vx0 + sht->bxsize, old_vy0 + sht->bysize);
+		sheet_refreshsub(ctl, vx0, vy0, vx0 + sht->bxsize, vy0 + sht->bysize);
 	}
 	return;
 }
